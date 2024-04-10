9ce3e076f4ac9c8058193060f570697ad6d50f44
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2024-04-10 23:51:04 +0800

    Add files via upload

---
+++ gen_list.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+dataset = 'VISDA-C'
+
+
+# In[3]:
+
+
+if dataset == 'office':
+	domains = ['amazon', 'dslr', 'webcam']
+elif dataset == 'office-caltech':
+	domains = ['amazon', 'dslr', 'webcam', 'caltech']
+elif dataset == 'office-home':
+	domains = ['Art', 'Clipart', 'Product', 'Real_World']
+elif dataset == 'image-clef':
+	domains = ['c', 'i', 'p']
+elif dataset == 'VISDA-C':
+	domains = ['train', 'validation']
+else:
+	print('No such dataset exists!')
+
+
+# In[4]:
+
+
+for domain in domains:
+	log = open(dataset+'/'+domain+'_list.txt','w')
+	directory = os.path.join(dataset, os.path.join(domain,'images'))
+	classes = [x[0] for x in os.walk(directory)]
+	classes = classes[1:]
+	classes.sort()
+	for idx,f in enumerate(classes):
+		files = os.listdir(f)
+		for file in files:
+			s = os.path.abspath(os.path.join(f,file)) + ' ' + str(idx) + '\n'
+			log.write(s)
+	log.close()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
